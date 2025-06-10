bd38c476959df8657706c7085dabbd39b84f6ab5
Sergio <principiante.en.programar@gmail.com>
2025-06-09 22:25:54 -0300

    change voice for quesitocalientedev x2

--- index.js
+++ index.js
@@ -134,7 +134,7 @@ const getInstructions = (voice) => {
         case 'LcfcDJNUP1GQjkzn1xUU':
             return {
                 voice: 'shimmer',
-                instructions: "una voz femenina con un toque de sensualidad,carismatica, con un acento colombiana bien marcada y que tenga una voz dulce y amorosa"
+                instructions: "una voz femenina con un toque de sensualidad,carismatica, con un acento colombiana bien marcada y paisa, que se note el acento colombiano y paisa, y que tenga una voz dulce y amorosa"
             };
         case 'piTKgcLEGmPE4e6mEKli':
             return {
